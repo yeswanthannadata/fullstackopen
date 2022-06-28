5549c5b45d0e692768b4d96c7ee57c43caedffb2
Yeshu <yeswanth@flutura.com>
2022-06-28 14:08:56 +0530

    course info part 4 changes

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,37 +13,43 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part content={props.part1} />
-      <Part content={props.part2} />
-      <Part content={props.part3} />
+      <Part content={props.parts[0]} />
+      <Part content={props.parts[1]} />
+      <Part content={props.parts[2]} />
     </>
   );
 };
 
 const Total = (props) => {
-  return <p>No of Exercises {props.total}</p>;
+  const total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+  return <p>No of Exercises {total}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header heading={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
