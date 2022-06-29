32004ead3566ef7709678213cc31ad044c40b746
Yeshu <yeswanth@flutura.com>
2022-06-29 12:56:36 +0530

    unicafe step6 changes

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,9 +6,10 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text}: {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -23,15 +24,16 @@ const Statistics = ({ good, neutral, bad }) => {
     );
   } else {
     return (
-      <>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={(good - bad) / all} />
-        <StatisticLine text="positive" value={(good / all) * 100} />
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={(good - bad) / all} />
+          <StatisticLine text="positive" value={(good / all) * 100} />
+        </tbody>
+      </table>
     );
   }
 };
