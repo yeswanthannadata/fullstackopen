6126f87cb5658cdc2633b854e775aa5d37cb17d2
Yeshu <yeswanth@flutura.com>
2022-06-29 11:53:21 +0530

    initial setup and unicafe step1 complete

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
