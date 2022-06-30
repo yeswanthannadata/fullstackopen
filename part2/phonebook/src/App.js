db30a385c589197228c3c054e5171b03b7df4ee9
Yeshu <yeswanth@flutura.com>
2022-06-30 14:46:39 +0530

    phonebook step 1 changes

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const App = () => {
+  const [persons, setPersons] = useState([{ name: "Yeswanth" }]);
+  const [newName, setNewName] = useState("");
+
+  const newNameHandler = (event) => {
+    event.preventDefault();
+    const newEntry = { name: newName };
+    setPersons(persons.concat(newEntry));
+    setNewName("");
+  };
+
+  const nameChangeHandler = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
-      <h1>Hello, React</h1>
+      <h2>Phonebook</h2>
+      <form onSubmit={newNameHandler}>
+        <div>
+          name: <input value={newName} onChange={nameChangeHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
