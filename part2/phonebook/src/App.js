96c71a31f4254a294dfdcad3c4ca3475fb5b13b1
Yeshu <yeswanth@flutura.com>
2022-06-30 19:24:13 +0530

    phonebook step3 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,23 +1,35 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Yeswanth" }]);
+  const [persons, setPersons] = useState([
+    { name: "Yeswanth", number: "91-1200938345" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [number, setNumber] = useState("");
 
   const newNameHandler = (event) => {
     event.preventDefault();
     const isExist = persons.some((person) => person.name === newName);
+    const isNumberExist = persons.some((person) => person.number === number);
+
     if (isExist) {
       alert(`${newName} is already added to phonebook`);
+    } else if (isNumberExist) {
+      alert(`${number} is already added to phonebook`);
     } else {
-      const newEntry = { name: newName };
+      const newEntry = { name: newName, number: number };
       setPersons(persons.concat(newEntry));
     }
     setNewName("");
+    setNumber("");
   };
 
   const nameChangeHandler = (event) => {
     setNewName(event.target.value);
+  };
+
+  const numberChangeHandler = (event) => {
+    setNumber(event.target.value);
   };
 
   return (
@@ -28,12 +40,17 @@ const App = () => {
           name: <input value={newName} onChange={nameChangeHandler} />
         </div>
         <div>
+          number: <input value={number} onChange={numberChangeHandler} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
