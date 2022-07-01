367a81cb8cc60f6032a93aed1d916faa1309fafa
Yeshu <yeswanth@flutura.com>
2022-07-01 20:04:54 +0530

    countries app step 3 complete

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,9 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const CountryListing = ({ countries, query, selectCountry }) => {
-  if (countries.length === 1) {
-    return <CountryDetail country={countries[0]} />;
-  } else if (countries.length > 10 && query) {
+  if (countries.length > 10 && query) {
     return <p>Too many matches, specify another filter</p>;
   } else if (countries.length <= 10) {
     return countries.map((country) => (
@@ -38,16 +36,41 @@ const CountryDetail = ({ country }) => {
   );
 };
 
+const WeatherDetail = ({ capital, weather }) => {
+  const iconUrl = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+  console.log(weather);
+  return (
+    <>
+      <h1>Weather in {capital}</h1>
+      <p>temperature {weather.main.temp} Celcius</p>
+      <img src={iconUrl} alt="Weather icon" />
+      <p>wind {weather.wind.speed} m/s</p>
+    </>
+  );
+};
+
 const App = () => {
   const [query, setQuery] = useState("");
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("");
+  const [weather, setWeather] = useState("");
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    if (country) {
+      const query = country.capital[0];
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&mode=json&units=metric&appid=${apiKey}`;
+      axios.get(url).then((response) => {
+        setWeather(response.data);
+      });
+    }
+  }, [country]);
 
   const filteredCountries = countries.filter((country) => {
     if (country.name.common.toLowerCase().includes(query.toLowerCase())) {
@@ -63,6 +86,7 @@ const App = () => {
   const queryChange = (event) => {
     setQuery(event.target.value);
     setCountry("");
+    setWeather("");
   };
 
   return (
@@ -77,6 +101,9 @@ const App = () => {
         />
       )}
       {country && <CountryDetail country={country} />}
+      {weather && (
+        <WeatherDetail capital={country.capital[0]} weather={weather} />
+      )}
     </div>
   );
 };
