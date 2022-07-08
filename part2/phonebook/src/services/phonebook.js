198c2c0d9c536aa1f7b06cf99854966eec24653e
Yeshu <yeswanth@flutura.com>
2022-07-08 09:51:59 +0530

    phonebook step9 and step11

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `http://localhost:3001/persons`;
+const baseUrl = `https://secret-coast-15373.herokuapp.com/api/persons`;
 
 const getAll = () => {
   const request = axios.get(baseUrl);
